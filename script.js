3a534529a943b7963467ee3917f0fb39e81c3cd5
SnowSlurpie <runniale000@gmail.com>
2022-03-16 14:48:03 -0500

    cleaned up style and began outlining necessary data in script

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// Assignment Code
+//Data Pools that'll be necessary to generate the Very Strong Password for the assignment!
+
+var choicePool = [];
+var passPool = [];
+var stringPool = "nemesis"
+
+
+//Data that are needed for the pools
+
+var uCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q','r','s','t','u','v','w','x','y','z'];
+var num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['!', '$', '?', '@', '%', '&'];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
